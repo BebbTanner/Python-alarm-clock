d104a13d2d0f60b0e4a81520fe210e6e1697fc28
TannerBebb <17tbebb@gmail.com>
2025-06-20 08:34:48 -0500

    I added some documentation.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 def updateTime():
     while True:
-        current_time = datetime.now().strftime("%H:%M")
+        update_time = datetime.now().strftime("%H:%M")
         time.sleep(1)
 
 current_time = datetime.now().strftime("%H:%M")
@@ -24,6 +24,10 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+"""
+I need to figure out what this stringVar does, I might need another one for my update funtion.
+This one should currently be the master stringvar since it has no parameters.
+"""
 alarm = StringVar()
 alarm_entry = ttk.Entry(mainframe, width=7, textvariable=alarm)
 alarm_entry.grid(column=2, row=1, sticky=(W, E))
@@ -31,6 +35,12 @@ alarm_entry.grid(column=2, row=1, sticky=(W, E))
 ttk.Button(mainframe, text="Set Alarm").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="What time?").grid(column=1, row=1, sticky=W)
 
+"""
+These are labels that are displayed on the GUI.
+The first one displays text that says current time 
+The second will eventually display the current time with an update function
+The Third one displays CST for the central timezone.
+"""
 ttk.Label(mainframe, text=" Current time: ").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text=current_time).grid(column=2, row=2, sticky=E)
 ttk.Label(mainframe, text= " CST ").grid(column=3, row=2, sticky=E)
