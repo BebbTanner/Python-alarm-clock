560236196701591b37a63187763b8eb01964d8be
TannerBebb <17tbebb@gmail.com>
2025-06-22 12:47:43 -0500

    This is a Template that will run. I need to create a branch and work off of this.

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 """
 Alarm clock mini project June 19th 2025 10:44
-
-I will be starting by just practicing with the documentation examples provided.
-
-CURRENT OBJECTIVES:
-Create a label that can hold that the updateTime function and display it on the GUI.
-Create a function that will check the alarm time with the current time.
-Create a function that will display a pop up that alarm is finish.
 """
 
 import time
@@ -14,23 +7,14 @@ from tkinter import *
 from tkinter import ttk
 from datetime import datetime
 
-"""
-    This is the update time function. This funciton is using a while loop that will update the current time
-and store it in the update_time variable. Using time.sleep(), it waits for one second to run the loop again. 
-In theory this should update the time every minute. The format is: HH:MM.
-"""
 def updateTime(*args):
-        
-        while True:
-            displayTime = datetime.now().strftime("%H:%M:%S")
-            time.sleep(1)
-            clock.set(displayTime)
 
-"""
-    This is the setAlarm function. This should be taking whatever value the user enters 
-and storing it in the alarmTime variable. I want to display this on a label in the mainframe
-window.
-"""
+    #clock = datetime.now().strftime("%H:%M:%S")
+
+    while True:
+        displayTime = datetime.now().strftime("%H:%M:%S")
+        time.sleep(1)
+
 def setAlarm(*args):
     try:
         alarmTime = float(alarm.get())
@@ -39,73 +23,29 @@ def setAlarm(*args):
     except ValueError:
         pass
 
-"""
-    This is the current time variable that will hold the current time on the machine. 
-The format is: HH:MM. The datetime.now() funciton is getting the current date and time.
-The strftime() is parsing the hour and minute data. This is the only data that I want.
-"""
-
-"""
-    I am unsure what this does right now.
-"""
 root = Tk()
 root.title("Alarm Clock")
 
-"""
-    It seems like the mainframe is basically just the window that will pop up that 
-will allow me to display things on. I see this as something like a table and the
-labels, buttons, ect are the plates being placed on the table.
-"""
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-"""
-    I need to figure out what this stringVar does, I might need another one for my update function.
-This one should currently be the master stringvar since it has no parameters.
-"""
 alarm = StringVar()
 alarm_entry = ttk.Entry(mainframe, width=7, textvariable=alarm)
 alarm_entry.grid(column=2, row=1, sticky=(W, E))
 currentAlarm = StringVar()
 
-clock = StringVar()
-
-"""
-    This is a tkinter button that will eventually be linked to a function that will
-allow the user to set an alarm.
-"""
-ttk.Button(mainframe, text="Set Alarm", command=setAlarm).grid(column=3, row=1, sticky=W)
-
-"""
-    These are labels that are displayed on the GUI.
-The first one displays text that says what time 
-The second one displays tex that says alarm set for.
-The Third one displays the value stored in the curretAlarm variable.
-"""
 ttk.Label(mainframe, text="What time?").grid(column=1, row=1, sticky=W)
 ttk.Label(mainframe, text= " Alarm set for: ").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text= " Current time: ").grid(column=1, row=3, sticky=E)
 ttk.Label(mainframe, textvariable=currentAlarm).grid(column=2, row=2, sticky=E)
+ttk.Label(mainframe, text= " (UPDATE TIME VARIABLE) ").grid(column=2, row=3, sticky=E)
+ttk.Button(mainframe, text="Set Alarm", command=setAlarm).grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text= " CST ").grid(column=3, row=3, sticky=E)
 
-"""
-    This is a for loop that is checking for every child class in the mainframe.
-It is then taking those child classes and adjusting them to the padding that is specified.
-Basically, if you want to change the spacing between each of the labels or buttons, 
-change the padding variables in this loop.
-"""
 for child in mainframe.winfo_children(): 
     child.grid_configure(padx=10, pady=10)
 
-"""
-    This is just calling the mainloop function that will run all of the related 
-code from above.
-"""
-#alarm_entry.focus()
-#root.mainloop()
-
-"""
-    This is just here to test my update time funciton. I will leave it in
-just in case I need it for later.
-"""
-updateTime()
+alarm_entry.focus()
+root.mainloop()
