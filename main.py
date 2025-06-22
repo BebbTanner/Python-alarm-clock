a473127ae472bb7e3aad521aad9e48c18e1efc39
TannerBebb <17tbebb@gmail.com>
2025-06-22 13:49:10 -0500

    I updated the setAlarm function. It will now store the time as a string rather that storing it as an integer. It also still displays the the GUI correctly.

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ from tkinter import *
 from tkinter import ttk
 from datetime import datetime
 
-def checkAlarm():
+"""
+    My current_time variable is stored as a string data type, and my alarmTime is stored as an integer.
+These are going to need to be the same data type in order to compare them.
+"""
+#def checkAlarm():
 
 
 def update_time():
@@ -25,8 +29,8 @@ def update_time():
 
 def setAlarm(*args):
     try:
-        alarmTime = float(alarm.get())
-        currentAlarm.set(int(alarmTime))
+        alarmTime = alarm.get()
+        currentAlarm.set(alarmTime)
         
     except ValueError:
         pass
@@ -40,15 +44,14 @@ root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 alarm = StringVar()
-alarm_entry = ttk.Entry(mainframe, width=7, textvariable=alarm)
-alarm_entry.grid(column=2, row=1, sticky=(W, E))
-currentAlarm = StringVar()
-
 time_var = StringVar()
+currentAlarm = StringVar()
+alarm_entry = ttk.Entry(mainframe, width=7, textvariable=alarm)
 
 ttk.Label(mainframe, text="What time?").grid(column=1, row=1, sticky=W)
 ttk.Label(mainframe, text= " Alarm set for: ").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text= " Current time: ").grid(column=1, row=3, sticky=E)
+alarm_entry.grid(column=2, row=1, sticky=(W, E))
 ttk.Label(mainframe, textvariable=currentAlarm).grid(column=2, row=2, sticky=E)
 time_label = ttk.Label(mainframe, textvariable=time_var).grid(column=2, row=3, sticky=E)
 ttk.Button(mainframe, text="Set Alarm", command=setAlarm).grid(column=3, row=1, sticky=W)
